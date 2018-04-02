b2a7b4bd0369a306a6304623fc1534d3f1b4ae42
Dušan Dudarin <nsdudo@yahoo.com>
2018-04-02 18:31:54 +0200

    Izmena modela

--- agent/src/main/java/com/agent/domain/Log.java
+++ agent/src/main/java/com/agent/domain/Log.java
@@ -6,19 +6,19 @@ public class Log {
 
 	private Long id;
 
-	private String priority;
+	//private String priority;
 
-	private String version;
+	//private String version;
 
 	private Date timeStamp;
 
-	private String hostName;
+	//private String hostName;
 
-	private String application;
+	//private String application;
 
-	private Long processId;
+	//private Long processId;
 
-	private Long MessageId;
+	//private Long MessageId;
 
 	private Agent agent;
 
@@ -26,44 +26,16 @@ public class Log {
 		super();
 	}
 
-	public Log(Long id, String priority, String version, Date timeStamp,
-			String hostName, String application, Long processId,
-			Long messageId, Agent agent) {
+	public Log(Long id, Date timeStamp, Agent agent) {
 		super();
 		this.id = id;
-		this.priority = priority;
-		this.version = version;
-		this.timeStamp = timeStamp;
-		this.hostName = hostName;
-		this.application = application;
-		this.processId = processId;
-		MessageId = messageId;
+        this.timeStamp=timeStamp;
 		this.agent = agent;
 	}
 
-	public Long getId() {
-		return id;
-	}
 
-	public void setId(Long id) {
-		this.id = id;
-	}
 
-	public String getPriority() {
-		return priority;
-	}
 
-	public void setPriority(String priority) {
-		this.priority = priority;
-	}
-
-	public String getVersion() {
-		return version;
-	}
-
-	public void setVersion(String version) {
-		this.version = version;
-	}
 
 	public Date getTimeStamp() {
 		return timeStamp;
@@ -71,38 +43,6 @@ public class Log {
 
 	public void setTimeStamp(Date timeStamp) {
 		this.timeStamp = timeStamp;
-	}
-
-	public String getHostName() {
-		return hostName;
-	}
-
-	public void setHostName(String hostName) {
-		this.hostName = hostName;
-	}
-
-	public String getApplication() {
-		return application;
-	}
-
-	public void setApplication(String application) {
-		this.application = application;
-	}
-
-	public Long getProcessId() {
-		return processId;
-	}
-
-	public void setProcessId(Long processId) {
-		this.processId = processId;
-	}
-
-	public Long getMessageId() {
-		return MessageId;
-	}
-
-	public void setMessageId(Long messageId) {
-		MessageId = messageId;
 	}
 
 	public Agent getAgent() {
@@ -115,11 +55,9 @@ public class Log {
 
 	@Override
 	public String toString() {
-		return "Log [id=" + id + ", priority=" + priority + ", version="
-				+ version + ", timeStamp=" + timeStamp + ", hostName="
-				+ hostName + ", application=" + application + ", processId="
-				+ processId + ", MessageId=" + MessageId + ", agent=" + agent
-				+ "]";
+		return "Log [id=" + id + ", timeStamp=" + timeStamp + ", agent=" + agent + "]";
 	}
+
+
 
 }
