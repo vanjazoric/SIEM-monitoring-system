b7fe552626de205b9d1f59b173c0482a65ba9830
vanja_1231 <vanja_zoric@hotmail.com>
2018-04-03 01:00:14 +0200

    Loaded logs from operating system

--- agent/src/main/java/com/agent/controller/AgentController.java
+++ agent/src/main/java/com/agent/controller/AgentController.java
@@ -4,8 +4,6 @@ package com.agent.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.agent.domain.Agent;
-
 
 @RestController
 @RequestMapping(value = "/agent")
