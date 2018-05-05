f8fb72ebaccaac25080459ec0cb439b7d26fb927
vanja_1231 <vanja_zoric@hotmail.com>
2018-05-05 16:49:55 +0200

    MongoDB

--- center/src/main/java/com/center/domain/Agent.java
+++ center/src/main/java/com/center/domain/Agent.java
@@ -2,8 +2,7 @@ package com.center.domain;
 
 import java.util.Set;
 
-import javax.persistence.Id;
-
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
